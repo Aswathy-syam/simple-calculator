f553bfa4abea414a36b380aa6f59a22bda11ce71
aswathy <syam05aswathy@gmail.com>
2023-10-31 19:10:44 +0530

    simple calculator

---
+++ calcu.js
@@ -0,0 +1,17 @@
+function display(num){
+    output.value+=num;  
+    // id.value=to get value
+}
+
+function clearAll(){
+    output.value=''
+}
+
+function evaluateExp(){
+    output.value=eval(output.value)
+}
+
+function removeLast(){
+    currentExp=output.value
+    output.value=currentExp.slice(0,-1)
+}
